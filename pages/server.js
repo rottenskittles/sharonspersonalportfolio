62b2c7bbf3386f8d9cb816d5ff7067e6f5db3e23
sharon <143832438+rottenskittles@users.noreply.github.com>
2024-05-08 09:50:08 -0700

    rdhewfhofoie

--- pages/server.js
+++ pages/server.js
@@ -8,3 +8,26 @@ function showPage() {
   document.getElementById("loader").style.display = "none";
   document.getElementById("myDiv").style.display = "block";
 }
+
+function toggleVis(elementID){
+  document.getElementById(elementID).classList.toggle("hidden");
+}
+document.addEventListener("DOMContentLoaded", function () {
+  let buttons = document.getElementsByClassName("nextBtn");
+  for (button of buttons) {
+    button.addEventListener("click", function (e) {
+
+      let btnVal = e.target.value;
+      console.log(e.target)
+      switch (btnVal) {
+        case "tointro":
+          console.log("tointro")
+          toggleVis('start');
+          toggleVis('intro');
+          break;
+
+      }
+    })
+  }
+
+})
